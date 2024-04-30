e021a60335baebe59e517a5aa5322ba65a993311
Cecilia Gårdebäck <ciz72@hotmail.com>
2024-04-30 08:23:10 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string color, pluralNoum, celebrity;
+
+Console.Write("Enter a color: ");
+color = Console.ReadLine();
+
+Console.Write("Enter a plural noum: ");
+pluralNoum = Console.ReadLine();
+
+Console.Write("Enter a celebrity: ");
+celebrity = Console.ReadLine();
+
+Console.WriteLine("Roses are " + color);
+Console.WriteLine(pluralNoum + " are blue");
+Console.WriteLine("I Love " +  celebrity);
+
+Console.ReadLine();
