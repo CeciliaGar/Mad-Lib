a8fe4e8eb6edef3c4e1e00b2282ff69d5d4de085
Cecilia Gårdebäck <ciz72@hotmail.com>
2024-04-30 08:42:08 +0200

    Calculator

--- Program.cs
+++ Program.cs
@@ -1,18 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-string color, pluralNoum, celebrity;
+Console.Write("Enter a number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter another number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Enter a color: ");
-color = Console.ReadLine();
-
-Console.Write("Enter a plural noum: ");
-pluralNoum = Console.ReadLine();
-
-Console.Write("Enter a celebrity: ");
-celebrity = Console.ReadLine();
-
-Console.WriteLine("Roses are " + color);
-Console.WriteLine(pluralNoum + " are blue");
-Console.WriteLine("I Love " +  celebrity);
+Console.WriteLine(num1 + num2);
 
 Console.ReadLine();
